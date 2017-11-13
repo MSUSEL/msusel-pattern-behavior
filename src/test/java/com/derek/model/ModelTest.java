0425dd1ae3715f96d4e2c042ea5938c56953c6e9
Derek Reimanis <derek.reimanis@gmail.com>
2017-11-13 11:06:34 -0700

    Integrated a Guava Graph to build individual pattern evolutions.
    Having a ton of issues with the jtable selections. Might be better to just use a tab, where each tab is a SoftwareVersion, and then to tab between them....

---
+++ src/test/java/com/derek/model/ModelTest.java
@@ -0,0 +1,19 @@
+package com.derek.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ModelTest {
+
+    @Before
+    public void buildMockObjects(){
+
+    }
+
+    @Test
+    public void buildPatternEvolution() throws Exception {
+    }
+
+}
