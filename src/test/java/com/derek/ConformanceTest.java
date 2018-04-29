502e0eb8d7a0303adc447bbddb3e417b13691c19
Derek Reimanis <derek.reimanis@gmail.com>
2018-04-29 10:54:53 -0600

    in the middle of test refactoring, but there is an issue setting up objects using the package tree data structure. I need to return to this.

--- src/test/java/com/derek/ConformanceTest.java
+++ src/test/java/com/derek/ConformanceTest.java
@@ -48,10 +48,10 @@ public class ConformanceTest {
 
     public StatePattern buildOneStateOneRequest(){
         List<Pair<String, String>> patternRoles = new ArrayList<>();
-        patternRoles.add(new Pair<>("Context", "CH.ifa.draw.applet.DrawApplet"));
-        patternRoles.add(new Pair<>("State", "CH.ifa.draw.framework.Tool"));
-        patternRoles.add(new Pair<>("state", "CH.ifa.draw.applet.DrawApplet::fTool:CH.ifa.draw.framework.Tool"));
-        patternRoles.add(new Pair<>("Request()", "CH.ifa.draw.applet.DrawApplet::setTool(CH.ifa.draw.framework.Tool, java.lang.String):void"));
+        patternRoles.add(new Pair<>("Context", "CH.DrawApplet"));
+        patternRoles.add(new Pair<>("State", "CH.Tool"));
+        patternRoles.add(new Pair<>("state", "CH.DrawApplet::fTool:CH.Tool"));
+        patternRoles.add(new Pair<>("Request()", "CH.DrawApplet::setTool(CH.Tool, java.lang.String):void"));
         PatternInstance pi = new PatternInstance(patternRoles, PatternType.STATE, new SoftwareVersion(1));
 
         UMLClassDiagram umlClassDiagram = new UMLClassDiagram();
@@ -64,23 +64,24 @@ public class ConformanceTest {
         drawAppletOps.add(new UMLOperation("setTool", params, "void"));
         List<String> residingPackage = new ArrayList<>();
         residingPackage.add("CH");
-        UMLClass drawApplet = new UMLClass("DrawApplet", null, null, drawAppletAttributes, drawAppletOps,
-                null, false, null, null, "class");
-        UMLClass tool = new UMLClass("Tool", null, null, null, null,
-                null, false, null, null, "class");
+        UMLClass drawApplet = new UMLClass("DrawApplet", new ArrayList<>(), new ArrayList<>(), drawAppletAttributes, drawAppletOps,
+                new ArrayList<>(), false, new ArrayList<>(), new ArrayList<>(), "class");
+        UMLClass tool = new UMLClass("Tool", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
+                new ArrayList<>(), false, new ArrayList<>(), new ArrayList<>(), "class");
         umlClassDiagram.addClassToDiagram(drawApplet);
         umlClassDiagram.addClassToDiagram(tool);
         umlClassDiagram.addRelationshipToDiagram(drawApplet, tool, Relationship.ASSOCIATION);
+        umlClassDiagram.buildPackageTree();
         return new StatePattern(pi, umlClassDiagram);
     }
 
     public StatePattern buildTwoStateOneRequest(){
         List<Pair<String, String>> patternRoles = new ArrayList<>();
-        patternRoles.add(new Pair<>("Context", "CH.ifa.draw.figures.LineConnection"));
-        patternRoles.add(new Pair<>("State", "CH.ifa.draw.framework.Connector"));
-        patternRoles.add(new Pair<>("state", "CH.ifa.draw.figures.LineConnection::fStart:CH.ifa.draw.framework.Connector"));
-        patternRoles.add(new Pair<>("state", "CH.ifa.draw.figures.LineConnection::fEnd:CH.ifa.draw.framework.Connector"));
-        patternRoles.add(new Pair<>("Request()", "CH.ifa.draw.figures.LineConnection::updateConnection():void"));
+        patternRoles.add(new Pair<>("Context", "CH.LineConnection"));
+        patternRoles.add(new Pair<>("State", "CH.Connector"));
+        patternRoles.add(new Pair<>("state", "CH.LineConnection::fStart:CH.Connector"));
+        patternRoles.add(new Pair<>("state", "CH.LineConnection::fEnd:CH.Connector"));
+        patternRoles.add(new Pair<>("Request()", "CH.LineConnection::updateConnection():void"));
         PatternInstance pi = new PatternInstance(patternRoles, PatternType.STATE, new SoftwareVersion(1));
 
         UMLClassDiagram umlClassDiagram = new UMLClassDiagram();
@@ -90,24 +91,27 @@ public class ConformanceTest {
         List<UMLOperation> contextOps = new ArrayList<>();
         List<Pair<String, String>> params = new ArrayList<>();
         contextOps.add(new UMLOperation("updateConnection", params, "void"));
-        UMLClass lineConnection = new UMLClass("LineConnection", null, null, contextAttributes, contextOps,
-                null, false, null, null, "class");
-        UMLClass connector = new UMLClass("Connector", null, null, null, null,
-                null, false, null, null, "class");
+        List<String> residingPackage = new ArrayList<>();
+        residingPackage.add("CH");
+        UMLClass lineConnection = new UMLClass("LineConnection", residingPackage, new ArrayList<>(), contextAttributes, contextOps,
+                new ArrayList<>(), false, new ArrayList<>(), new ArrayList<>(), "class");
+        UMLClass connector = new UMLClass("Connector", residingPackage, new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
+                new ArrayList<>(), false, new ArrayList<>(), new ArrayList<>(), "class");
         umlClassDiagram.addClassToDiagram(lineConnection);
         umlClassDiagram.addClassToDiagram(connector);
         umlClassDiagram.addRelationshipToDiagram(lineConnection, connector, Relationship.ASSOCIATION);
+        umlClassDiagram.buildPackageTree();
         return new StatePattern(pi, umlClassDiagram);
     }
 
 
     public StatePattern buildOneStateTwoRequest(){
         List<Pair<String, String>> patternRoles = new ArrayList<>();
-        patternRoles.add(new Pair<>("Context", "CH.ifa.draw.standard.ConnectionHandle"));
-        patternRoles.add(new Pair<>("State", "CH.ifa.draw.framework.Figure"));
-        patternRoles.add(new Pair<>("state", "CH.ifa.draw.standard.ConnectionHandle::fTarget:CH.ifa.draw.framework.Figure"));
-        patternRoles.add(new Pair<>("Request()", "CH.ifa.draw.standard.ConnectionHandle::invokeEnd():void"));
-        patternRoles.add(new Pair<>("Request()", "CH.ifa.draw.standard.ConnectionHandle::invokeStep():void"));
+        patternRoles.add(new Pair<>("Context", "CH.ConnectionHandle"));
+        patternRoles.add(new Pair<>("State", "CH.Figure"));
+        patternRoles.add(new Pair<>("state", "CH.ConnectionHandle::fTarget:CH.Figure"));
+        patternRoles.add(new Pair<>("Request()", "CH.ConnectionHandle::invokeEnd():void"));
+        patternRoles.add(new Pair<>("Request()", "CH.ConnectionHandle::invokeStep():void"));
         PatternInstance pi = new PatternInstance(patternRoles, PatternType.STATE, new SoftwareVersion(1));
 
         UMLClassDiagram umlClassDiagram = new UMLClassDiagram();
@@ -117,24 +121,27 @@ public class ConformanceTest {
         List<Pair<String, String>> params = new ArrayList<>();
         contextOps.add(new UMLOperation("invokeEnd", params, "void"));
         contextOps.add(new UMLOperation("invokeStep", params, "void"));
-        UMLClass connectionHandle = new UMLClass("ConnectionHandle", null, null, contextAttributes, contextOps,
-                null, false, null, null, "class");
-        UMLClass figure = new UMLClass("Figure", null, null, null, null,
-                null, false, null, null, "class");
+        List<String> residingPackage = new ArrayList<>();
+        residingPackage.add("CH");
+        UMLClass connectionHandle = new UMLClass("ConnectionHandle", residingPackage, new ArrayList<>(), contextAttributes, contextOps,
+                new ArrayList<>(), false, new ArrayList<>(), new ArrayList<>(), "class");
+        UMLClass figure = new UMLClass("Figure", residingPackage, new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
+                new ArrayList<>(), false, new ArrayList<>(), new ArrayList<>(), "class");
         umlClassDiagram.addClassToDiagram(connectionHandle);
         umlClassDiagram.addClassToDiagram(figure);
         umlClassDiagram.addRelationshipToDiagram(connectionHandle, figure, Relationship.ASSOCIATION);
+        umlClassDiagram.buildPackageTree();
         return new StatePattern(pi, umlClassDiagram);
     }
 
     public StatePattern buildTwoStateTwoRequest(){
         List<Pair<String, String>> patternRoles = new ArrayList<>();
-        patternRoles.add(new Pair<>("Context", "CH.ifa.draw.standard.CreationTool"));
-        patternRoles.add(new Pair<>("State", "CH.ifa.draw.framework.Figure"));
-        patternRoles.add(new Pair<>("state", "CH.ifa.draw.standard.CreationTool::fPrototype:CH.ifa.draw.framework.Figure"));
-        patternRoles.add(new Pair<>("state", "CH.ifa.draw.standard.CreationTool::fCreatedFigure:CH.ifa.draw.framework.Figure"));
-        patternRoles.add(new Pair<>("Request()", "CH.ifa.draw.standard.CreationTool::createFigure():CH.ifa.draw.framework.Figure"));
-        patternRoles.add(new Pair<>("Request()", "CH.ifa.draw.standard.CreationTool::mouseDown():void"));
+        patternRoles.add(new Pair<>("Context", "CH.CreationTool"));
+        patternRoles.add(new Pair<>("State", "CH.Figure"));
+        patternRoles.add(new Pair<>("state", "CH.CreationTool::fPrototype:CH.Figure"));
+        patternRoles.add(new Pair<>("state", "CH.CreationTool::fCreatedFigure:CH.Figure"));
+        patternRoles.add(new Pair<>("Request()", "CH.CreationTool::createFigure():CH.Figure"));
+        patternRoles.add(new Pair<>("Request()", "CH.CreationTool::mouseDown():void"));
         PatternInstance pi = new PatternInstance(patternRoles, PatternType.STATE, new SoftwareVersion(1));
 
         UMLClassDiagram umlClassDiagram = new UMLClassDiagram();
@@ -145,13 +152,16 @@ public class ConformanceTest {
         List<Pair<String, String>> params = new ArrayList<>();
         contextOps.add(new UMLOperation("createFigure", params, "Figure"));
         contextOps.add(new UMLOperation("mouseDown", params, "void"));
-        UMLClass creationTool = new UMLClass("CreationTool", null, null, contextAttributes, contextOps,
-                null, false, null, null, "class");
-        UMLClass figure = new UMLClass("Figure", null, null, null, null,
-                null, false, null, null, "class");
+        List<String> residingPackage = new ArrayList<>();
+        residingPackage.add("CH");
+        UMLClass creationTool = new UMLClass("CreationTool", residingPackage, new ArrayList<>(), contextAttributes, contextOps,
+                new ArrayList<>(), false, new ArrayList<>(), new ArrayList<>(), "class");
+        UMLClass figure = new UMLClass("Figure", residingPackage, new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
+                new ArrayList<>(), false, new ArrayList<>(), new ArrayList<>(), "class");
         umlClassDiagram.addClassToDiagram(creationTool);
         umlClassDiagram.addClassToDiagram(figure);
         umlClassDiagram.addRelationshipToDiagram(creationTool, figure, Relationship.ASSOCIATION);
+        umlClassDiagram.buildPackageTree();
         return new StatePattern(pi, umlClassDiagram);
     }
 
